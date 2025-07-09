6526a460ca59ce28802a847d35d610cebcb6c4bd
EDOV <alpredo.vandy@gmail.com>
2025-07-09 22:31:26 +0700

    layout: register header and footer

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="min-h-screen bg-gradient-to-br from-green-100 via-blue-100 to-purple-100 flex flex-col"
+  >
+    <!-- Header -->
+    <Header />
+
+    <!-- Main Content -->
+    <div class="flex-1">
+      <router-view />
+    </div>
+
+    <!-- Footer -->
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default defineComponent({
+  components: { Header, Footer },
+});
+</script>
