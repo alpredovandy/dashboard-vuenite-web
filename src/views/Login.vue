49f19468a68b73880ca456b295fbb560f6ae4023
EDOV <alpredo.vandy@gmail.com>
2025-07-09 22:31:58 +0700

    views: add view login, profile and notfound route

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="prose max-w-none">
+    <LoginFeature />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LoginFeature from "../features/Login/index.vue";
+
+export default defineComponent({
+  name: "LoginPage",
+  components: { LoginFeature },
+});
+</script>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="prose max-w-none">
+    <h1>Not Found.</h1>
+    <p>
+      You’ve landed on a URL that doesnt seem to exist. Return
+      <router-link to="/"> home </router-link>.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {},
+});
+</script>
+
+<style scoped>
+h1 {
+  margin-bottom: 0;
+}
+</style>
