464933736cb3fb4db2a688e20f7fa631b18c68d5
EDOV <alpredo.vandy@gmail.com>
2025-07-12 00:10:12 +0700

    utils: add isEmpty funct

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,15 @@
 export const getAvatarByName = (name: string) => {
   https: return `https://ui-avatars.com/api/?name=${name}&background=random`;
 };
+
+export const isEmpty = (str: unknown): boolean => {
+  return (
+    str === "" ||
+    str === null ||
+    str === undefined ||
+    str === "undefined" ||
+    !str ||
+    (Array.isArray(str) && !str.length) ||
+    (typeof str === "object" && Object.entries(str).length === 0)
+  );
+};
